4fbb59b3765dbd0723c357729f8bd859db0e4089
olivier <olivier.winter@hotmail.fr>
2019-10-23 21:07:14 +0100

    correct address for images

--- build_local.sh
+++ build_local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 CHEM=/home/olivier/Documents/jwaclassic
-SYNC_IMAGES=/home/olivier/OneDrive/_Flotte_JWA_Classic/00images_site
+SYNC_IMAGES=/home/olivier/OneDrive/_Flotte_JWA_Classic/00_images_site
 rsync -av --progress  $SYNC_IMAGES/* $CHEM/images/
 bundle exec jekyll serve --config _config.yml,_config_dev.yml --destination ./_site
 # docker-compose up
